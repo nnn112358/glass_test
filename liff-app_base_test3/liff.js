278ba79410534d38b3ae3c5cf725fa32f143be3e
nnn112358 <enoreplies@gmail.com>
2019-07-14 08:18:13 +0900

    Add files via upload

--- liff-app_base_test3/liff.js
+++ liff-app_base_test3/liff.js
@@ -263,7 +263,7 @@ function liffGetButtonStateCharacteristic(characteristic) {
 function liffToggleDeviceLedState(state) {
     // on: 0x01
     // off: 0x00
-    window.ledCharacteristic.writeValue(new Uint8Array([0x03])).catch(error => {
+    window.ledCharacteristic.writeValue(new Uint8Array([0x13])).catch(error => {
         uiStatusError(makeErrorMsg(error), false);
     });
 }
